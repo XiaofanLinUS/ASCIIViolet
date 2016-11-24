94c1e02084a558beb299e0b4a40a1b84ee5fd700
Gideon Ubaldo <gideonubaldo@Gideons-MacBook-Pro.local>
2016-11-24 14:02:49 -0800

    wip

--- com/cs151/asciiviolet/SequenceDiagramReader.java
+++ com/cs151/asciiviolet/SequenceDiagramReader.java
@@ -1,13 +1,13 @@
 package com.cs151.asciiviolet;
 
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.horstmann.violet.SequenceDiagramGraph;
 import com.horstmann.violet.framework.Edge;
-=======
->>>>>>> 02e2c7994a46a146c1feda0b2377a871d66aca69
+
+
 import com.horstmann.violet.framework.Graph;
 
 /**
@@ -18,23 +18,7 @@ import com.horstmann.violet.framework.Graph;
  */
 public class SequenceDiagramReader implements Reader
 {
-
-<<<<<<< HEAD
-
-	public SequenceDiagramReader(Graph aGraph) {
-		// TODO Auto-generated constructor stub
-	
-		   
-		 
-	}
-
-	@Override
-	public void read(String command) {
-		
-		
-	}
-	 
-=======
+ 
    /**
     * Construct a SequenceReader with a given graph
     * 
@@ -53,5 +37,5 @@ public class SequenceDiagramReader implements Reader
       // TODO Auto-generated method stub
 
    }
->>>>>>> 02e2c7994a46a146c1feda0b2377a871d66aca69
+
 }
