7fb1fc73737dd6f0c34221db97744216ba256142
Gideon Ubaldo <gideonubaldo@Gideons-MacBook-Pro.local>
2016-11-26 22:19:12 -0800

    wip

--- com/cs151/asciiviolet/SequenceDiagramReader.java
+++ com/cs151/asciiviolet/SequenceDiagramReader.java
@@ -1,14 +1,19 @@
 package com.cs151.asciiviolet;
 
 
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
+import com.horstmann.violet.ClassNode;
 import com.horstmann.violet.SequenceDiagramGraph;
 import com.horstmann.violet.framework.Edge;
 
 
 import com.horstmann.violet.framework.Graph;
+import com.horstmann.violet.framework.MultiLineString;
+import com.horstmann.violet.framework.Node;
 
 /**
  * A command reader that reads a string and execute it
@@ -27,15 +32,27 @@ public class SequenceDiagramReader implements Reader
     */
    public SequenceDiagramReader(Graph graph)
    {
-      // TODO Auto-generated constructor stub
-
+	   this.graph = graph; 
    }
 
    @Override
-   public void read(String command)
+   public void read(String input)
    {
-      // TODO Auto-generated method stub
-
+	  
+	   if(input.equals("hey")){
+		   ClassNode node1 = new ClassNode();
+	         MultiLineString string1 = new MultiLineString();
+	         string1.setText(input);
+	         node1.setName(string1);
+	         nodeA = node1;
+	         classNodes.put(input, nodeA);
+	         while (!graph.add(nodeA, new Point2D.Double(80 * Math.random(), 80 * Math.random())))
+	         {
+	         }
+	   }
    }
-
+   
+   private HashMap<String, Node> classNodes;
+   Node nodeA;
+   Graph graph;
 }
